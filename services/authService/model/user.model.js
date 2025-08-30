37f7567b4fbffb836e8f5deb1266fb50a88bdfac
Hamza Bashir <hmzabshr120@gmail.com>
2025-08-30 14:35:21 +0500

    user model schema

---
+++ services/authService/model/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    plainPassword:{
+        type:String,
+        required:true
+    },
+    hashPassword:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["user", "admin"],
+        default:"user"
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    },
+    resetPasswordToken:{
+        type:String,
+        default:null
+    },
+    resetPasswordExpires:{
+        type:Date,
+        default:null
+    }
+}, {
+    timestamps:true
+})
+
+
+module.exports = mongoose.model("user", userSchema)
